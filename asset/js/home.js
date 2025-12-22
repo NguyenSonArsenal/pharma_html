da99c79a6405dd5e9e7633503f08b498deee293f
sonnv <vanson297.nguyen@gmail.com>
2025-12-22 22:38:31 +0700

    add

---
+++ asset/js/home.js
@@ -0,0 +1,20 @@
+$( document ).ready(function() {
+  console.log( "ready!" );
+
+  const swiper = new Swiper('.swiper', {
+    loop: true,
+    autoplay: { delay: 4000, disableOnInteraction: false },
+    slidesPerView: 2,
+    spaceBetween: 24,
+    speed: 500,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    breakpoints: {
+      0: { slidesPerView: 1, spaceBetween: 16 },
+      768: { slidesPerView: 2, spaceBetween: 24 },
+    },
+  });
+});
+
